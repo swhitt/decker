75228420344d4e4480c17469740e02b8490072cf
Steve Whittaker <swhitt@gmail.com>
2017-06-13 18:04:50 -0500

    Add `the_cheaty_way.rb`
    If this wasn't for fun I'd probably do it this way.

---
+++ the_cheaty_way.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+require 'ruby-poker'
+
+# This is the way I'd probably do it in the real world (with some test coverage
+# of course) but it kind of defeats the point of the exercise, eh? :)
+# Requires the ruby gem `ruby-poker`, you can get it with `gem install ruby-poker`/
+
+scores = { player1: 0, player2: 0, tie: 0 }
+
+open('https://projecteuler.net/project/resources/p054_poker.txt') do |f|
+  f.each_line do |notation|
+    hand1 = PokerHand.new(notation[0..13])
+    hand2 = PokerHand.new(notation[15..28])
+
+    case hand1 <=> hand2
+    when 1
+      scores[:player1] += 1
+    when 0
+      scores[:tie] += 1
+    when -1
+      scores[:player2] += 1
+    end
+  end
+end
+
+puts scores.inspect
+# => {:player1=>376, :player2=>624, :tie=>0}
