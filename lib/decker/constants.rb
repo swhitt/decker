c5c26179bdd1b32db0fbb4c66a9f62283ed3286b
Steve Whittaker <swhitt@gmail.com>
2017-06-13 18:03:28 -0500

    Add some documentation to `constants.rb`
    Descriptions of how the various regular expressions are used, etc.

--- lib/decker/constants.rb
+++ lib/decker/constants.rb
@@ -1,4 +1,7 @@
 module Decker
+  # The rank of a Card is the number or face value it has. This
+  # maps the Symbol representation of rank to its English 
+  # representation.
   RANKS = {
     '2': 'Two',
     '3': 'Three',
@@ -15,6 +18,8 @@ module Decker
       A: 'Ace'
   }.freeze
 
+  # Similar to the RANKS hash, the SUITS hash maps the Symbol
+  # representation of a Card's suit to its English representation
   SUITS = {
     C: 'Clubs',
     D: 'Diamonds',
@@ -22,12 +27,22 @@ module Decker
     S: 'Spades'
   }.freeze
 
+  # A Map between the Suit symbols and some nice unicode
+  # symbols that represent the suits, suitable for display
+  # or used in #inspect
   PRETTY_SUITS = Hash[SUITS.keys.zip(%w[♣ ♦ ♥ ♠])].freeze
 
+  # a regular expression used to separate a Hand's full notation
+  # into individual Card notations.
   ONE_CARD_REGEXP = /[#{RANKS.keys.join}][#{SUITS.keys.join}]/
 
+  # a regular expression just to validate the notation for
+  # an entire hand.
   HAND_REGEXP = /\A(#{ONE_CARD_REGEXP}\s?)+\Z/
 
+  # the named capture groups in this regexp will allow us to 
+  # grab the rank and suit of the desired card quickly without
+  # messing with MatchData indices
   EXTRACT_RANK_AND_SUIT_REGEXP =
     /\A(?<rank>[#{RANKS.keys.join}])(?<suit>[#{SUITS.keys.join}])\Z/
 end
